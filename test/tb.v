466179f2bd98dcaa3c5262c81d5b38a351fc4cc4
mRoales <manuel.roales@imse-cnm.csic.es>
2026-06-17 09:23:32 +0200

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -28,7 +28,7 @@ module tb ();
 `endif
 
   // Replace tt_um_example with your module name:
-  tt_um_bch_decoder (
+  tt_um_bch_decoder dut (
 
       // Include power ports for the Gate Level test:
 `ifdef GL_TEST
